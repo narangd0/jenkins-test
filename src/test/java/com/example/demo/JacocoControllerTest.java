60f9e8eac1221ad9c7ca427f1ac75db40d94f037
narangd0 <squarearth1@gmail.com>
2023-01-02 19:40:29 +0900

    test

---
+++ src/test/java/com/example/demo/JacocoControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JacocoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void test() throws Exception {
+        mvc.perform(get("/test")
+                        .param("n", "3"))
+                .andExpect(content().string("hello"))
+                .andExpect(status().isOk());
+    }
+}
