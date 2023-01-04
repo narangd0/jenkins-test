a0538b2974afa6ec6473dc7634ab298a0c201c93
서민지 <squarearth1@gmail.com>
2023-01-04 11:18:59 +0900

    test

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String getHome() {
-        return "Hello World";
+        return "Hello World!";
     }
 }
